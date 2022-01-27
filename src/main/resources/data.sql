dc715e5e486450d80c3999cf6351b64efb5b0a4c
kho903 <gmldnr2222@naver.com>
2022-01-28 00:16:38 +0900

    refactor: data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 
 INSERT INTO QNA (id, category, phone_number, email, title, message, checked, create_date)
-VALUES (1, '농가입점', '010-1111-1111', 'gmldnr2222@naver.com', '농가입점', '농가입점문의입니다.', true , '2022-01-01 00:02:00.000000');
+VALUES (1, '제휴문의', '010-5013-6138', 'kho903@gmail.com',
+        '제 브랜드와 제휴하고 싶습니다.',
+        '뱅카우 측과 저희 브랜드가 만난다면 좋은 시너지를 발휘할 것입니다.',
+        true, '2022-01-01 00:02:00.000000');
 
