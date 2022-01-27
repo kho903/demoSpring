0fc4e126f62a202935f47b2ad88ca38100faab35
kho903 <gmldnr2222@naver.com>
2022-01-27 13:37:35 +0900

    first commit

---
+++ src/main/java/com/example/bancowdemo/BancowdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bancowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancowdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancowdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bancowdemo/BancowdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bancowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancowdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
